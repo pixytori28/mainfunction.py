aa02f564d9c7deec99bd00a30062dc6cb02428f1
Victoria Inahuazo <victoria.inahuazo11@myhunter.cuny.edu>
2024-04-01 15:45:43 -0400

    Add files via upload

---
+++ h31main.py
@@ -0,0 +1,10 @@
+#victoria inahuazo
+#victoria.inahuazo11@myhunter.cuny.edu
+#04/01/24 april fools day!!!!
+#this programs prints hellow world using main function
+    
+def main ():
+    print("Hello, World!")
+    
+if __name__ == "__main__":
+    main()
